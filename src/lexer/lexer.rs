a8a0ff428fc0e48c163d6cadb1ee900d5e8a643a
andrew644 <andrew644@users.noreply.github.com>
2026-01-15 23:20:49 -0500

    Add lexer

---
+++ src/lexer/lexer.rs
@@ -0,0 +1,3 @@
+pub fn lex(code: String) {
+    println!("Lexing");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+mod lexer;
+
+use lexer::lexer::lex;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_file(file_path: &String) {
+    let path = Path::new(file_path);
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {display}: {why}"),
+        Ok(file) => file,
+    };
+
+    let mut code = String::new();
+    match file.read_to_string(&mut code) {
+        Err(why) => panic!("Couldn't read {display}: {why}"),
+        Ok(_) => print!("{display} contains:\n{code}"),
+    }
+
+    lex(code);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        2 => {
+            let file = &args[1];
+            println!("Compiling {file}");
+            read_file(file);
+        }
+        _ => {
+            let binary_name = &args[0];
+            println!("Usage: {binary_name} file.dux");
+        }
+    }
 }
